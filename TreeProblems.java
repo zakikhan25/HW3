65b90605ad383b7534d97e9595a4b336b9738982
zakikhan25 <zkhan25@luc.edu>
2025-02-21 20:49:13 -0600

    Update TreeProblems.java

--- TreeProblems.java
+++ TreeProblems.java
@@ -1,8 +1,8 @@
 /*
- * *** YOUR NAME GOES HERE / YOUR SECTION NUMBER ***
+ * *** Zaki Khan / 400 001 ***
  *
  * This java file contains several simple tree problems that need to be
- * codified. These routines  must use the TreeMap and TreeSet library
+ * codified. These routines must use the TreeMap and TreeSet library
  * classes from the Java Collection Framework.
  *
  */
@@ -14,37 +14,35 @@ public class TreeProblems {
   /**
    * Method different()
    *
-   * Given two TreeSets of integers, return a TreeSet containing all elements 
+   * Given two TreeSets of integers, return a TreeSet containing all elements
    * that are NOT in both sets. In other words, return a TreeSet of all the
    * elements that are in one set but not the other.
    */
-  
+
   public static Set<Integer> different(Set<Integer> setA, Set<Integer> setB) {
-
-    // INSERT CODE HERE - DO NOT FORGET TO PLACE YOUR NAME ABOVE
-    //
-    // This can be done numerous ways, but once such will only that
-    // *several* lines of code. Hint: create two temporary TreeSets and utilize the
-    // methods retainAll(), addAll(), and removeAll(). But in the end, get something to work.
-
-    return setA;
+    Set<Integer> resultSet = new TreeSet<>(setA);
+    resultSet.addAll(setB);
+    Set<Integer> commonSet = new TreeSet<>(setA);
+    commonSet.retainAll(setB);
+    resultSet.removeAll(commonSet);
+    return resultSet;
   }
-
 
   /**
    * Method removeEven()
    *
    * Given a treeMap with the key as an integer, and the value as a String,
-   * remove all <key, value> pairs where the key is even. 
+   * remove all <key, value> pairs where the key is even.
    */
 
   public static void removeEven(Map<Integer, String> treeMap) {
-
-    // INSERT CODE HERE.
-
-    return;
+    Iterator<Integer> iterator = treeMap.keySet().iterator();
+    while (iterator.hasNext()) {
+      if (iterator.next() % 2 == 0) {
+        iterator.remove();
+      }
+    }
   }
-
 
   /**
    * Method treesEqual()
@@ -53,12 +51,16 @@ public class TreeProblems {
    * return a boolean value indicating if the two trees are equal or not.
    */
 
-  public boolean treesEqual(Map<Integer, String> tree1,Map<Integer, String> tree2 ) {
-
-    // INSERT CODE HERE
-
-    return false;
-
+  public boolean treesEqual(Map<Integer, String> tree1, Map<Integer, String> tree2) {
+    if (tree1.size() != tree2.size()) {
+      return false;
+    }
+    for (Map.Entry<Integer, String> entry : tree1.entrySet()) {
+      if (!entry.getValue().equals(tree2.get(entry.getKey()))) {
+        return false;
+      }
+    }
+    return true;
   }
 
-} // end treeProblems class
+} // end TreeProblems class
